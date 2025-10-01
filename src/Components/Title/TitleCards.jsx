e9e380eb54932fa126e75328fe47326a5e95b0fa
kiddowbn <nboikhantsho@gmail.com>
2025-10-01 17:09:06 +0200

    netflix-clone-commit_01
    added
    -assets
    -Components(titleCards-jsx+css)
    -Login(Login-jsx+css)
    -Page(home-jsx+css)
    -Player(Player-jsx+css)
    -vite intalled

---
+++ src/Components/Title/TitleCards.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './TitleCards.css'
+
+import React from 'react'
+
+const TitleCards = () => {
+  return (
+    <div className='TitleCards'>
+
+        
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/Page/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+export const home = () => {
+  return (
+    <div className='home' >
+
+
+    </div>
+  )
+}
